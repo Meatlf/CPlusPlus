d7ec8f07ba35ad82bff3369f2acae0e07367369b
Meatlf <454779758@qq.com>
2020-06-13 17:29:12 +0800

    temp 第11章练习题4还未做完

---
+++ Chapter11-使用类/exercise4_test.cpp
@@ -0,0 +1,23 @@
+//usetime4.cpp -- using the fourth draft of the Time class
+// compile usetime4.cpp and mytime4.cpp together
+#include <iostream>
+#include "mytime4.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    Time aida(3, 35);
+    Time tosca(2, 48);
+    Time temp;
+
+    cout << "Aida and Tosca:\n";
+    cout << aida<<"; " << tosca << endl;
+    temp = aida + tosca;     // operator+()
+    cout << "Aida + Tosca: " << temp << endl;
+    temp = aida* 1.17;  // member operator*()
+    cout << "Aida * 1.17: " << temp << endl;
+    cout << "10.0 * Tosca: " << 10.0 * tosca << endl;
+	// std::cin.get();
+    return 0; 
+}
