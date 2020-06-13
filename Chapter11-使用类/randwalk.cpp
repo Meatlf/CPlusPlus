62e50bb441ea3269a9ae5feccbe7319329554881
Meatlf <454779758@qq.com>
2020-06-13 16:17:21 +0800

    build 编译通过已学代码

--- Chapter11-使用类/randwalk.cpp
+++ Chapter11-使用类/randwalk.cpp
@@ -1,14 +1,14 @@
 // randwalk.cpp -- using the Vector class
 // compile with the vect.cpp file
 #include <iostream>
-#include <cstdlib>      // rand(), srand() prototypes
-#include <ctime>        // time() prototype
+#include <cstdlib> // rand(), srand() prototypes
+#include <ctime>   // time() prototype
 #include "vect.h"
 int main()
 {
     using namespace std;
     using VECTOR::Vector;
-    srand(time(0));     // seed random-number generator
+    srand(time(0)); // seed random-number generator
     double direction;
     Vector step;
     Vector result(0.0, 0.0);
@@ -25,27 +25,28 @@ int main()
         while (result.magval() < target)
         {
             direction = rand() % 360;
-            step.reset(dstep, direction, POL);
+            step.reset(dstep, direction, VECTOR::Vector::Mode::POL);
             result = result + step;
             steps++;
         }
         cout << "After " << steps << " steps, the subject "
-            "has the following location:\n";
+                                     "has the following location:\n";
         cout << result << endl;
         result.polar_mode();
-        cout << " or\n" << result << endl;
+        cout << " or\n"
+             << result << endl;
         cout << "Average outward distance per step = "
-            << result.magval()/steps << endl;
+             << result.magval() / steps << endl;
         steps = 0;
         result.reset(0.0, 0.0);
         cout << "Enter target distance (q to quit): ";
     }
     cout << "Bye!\n";
-/* keep window open
+    /* keep window open
     cin.clear();
     while (cin.get() != '\n')
         continue;
     cin.get();
 */
-    return 0; 
+    return 0;
 }
