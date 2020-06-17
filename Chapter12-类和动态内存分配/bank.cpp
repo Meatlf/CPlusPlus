01d4d998b74eefb5e1f7ca9d6b60b1dd0a19516b
Meatlf <454779758@qq.com>
2020-06-17 22:04:17 +0800

    style 对第12章 例程4进行代码格式化

--- Chapter12-类和动态内存分配/bank.cpp
+++ Chapter12-类和动态内存分配/bank.cpp
@@ -14,40 +14,39 @@ int main()
     using std::cout;
     using std::endl;
     using std::ios_base;
-// setting things up
-    std::srand(std::time(0));    //  random initializing of rand()
+    // setting things up
+    std::srand(std::time(0)); //  random initializing of rand()
 
     cout << "Case Study: Bank of Heather Automatic Teller\n";
     cout << "Enter maximum size of queue: ";
     int qs;
     cin >> qs;
-    Queue line(qs);         // line queue holds up to qs people
+    Queue line(qs); // line queue holds up to qs people
 
     cout << "Enter the number of simulation hours: ";
-    int hours;              //  hours of simulation
+    int hours; //  hours of simulation
     cin >> hours;
     // simulation will run 1 cycle per minute
     long cyclelimit = MIN_PER_HR * hours; // # of cycles
 
     cout << "Enter the average number of customers per hour: ";
-    double perhour;         //  average # of arrival per hour
+    double perhour; //  average # of arrival per hour
     cin >> perhour;
-    double min_per_cust;    //  average time between arrivals
+    double min_per_cust; //  average time between arrivals
     min_per_cust = MIN_PER_HR / perhour;
 
-    Item temp;              //  new customer data
-    long turnaways = 0;     //  turned away by full queue
-    long customers = 0;     //  joined the queue
-    long served = 0;        //  served during the simulation
-    long sum_line = 0;      //  cumulative line length
-    int wait_time = 0;      //  time until autoteller is free
-    long line_wait = 0;     //  cumulative time in line
+    Item temp;          //  new customer data
+    long turnaways = 0; //  turned away by full queue
+    long customers = 0; //  joined the queue
+    long served = 0;    //  served during the simulation
+    long sum_line = 0;  //  cumulative line length
+    int wait_time = 0;  //  time until autoteller is free
+    long line_wait = 0; //  cumulative time in line
 
-
-// running the simulation
+    // running the simulation
     for (int cycle = 0; cycle < cyclelimit; cycle++)
     {
-        if (newcustomer(min_per_cust))  // have newcomer
+        if (newcustomer(min_per_cust)) // have newcomer
         {
             if (line.isfull())
                 turnaways++;
@@ -60,7 +59,7 @@ int main()
         }
         if (wait_time <= 0 && !line.isempty())
         {
-            line.dequeue (temp);      // attend next customer
+            line.dequeue(temp);       // attend next customer
             wait_time = temp.ptime(); // for wait_time minutes
             line_wait += cycle - temp.when();
             served++;
@@ -70,7 +69,7 @@ int main()
         sum_line += line.queuecount();
     }
 
-// reporting results
+    // reporting results
     if (customers > 0)
     {
         cout << "customers accepted: " << customers << endl;
@@ -79,9 +78,9 @@ int main()
         cout << "average queue size: ";
         cout.precision(2);
         cout.setf(ios_base::fixed, ios_base::floatfield);
-        cout << (double) sum_line / cyclelimit << endl;
+        cout << (double)sum_line / cyclelimit << endl;
         cout << " average wait time: "
-             << (double) line_wait / served << " minutes\n";
+             << (double)line_wait / served << " minutes\n";
     }
     else
         cout << "No customers!\n";
@@ -95,5 +94,5 @@ int main()
 //  return value is true if customer shows up this minute
 bool newcustomer(double x)
 {
-    return (std::rand() * x / RAND_MAX < 1); 
+    return (std::rand() * x / RAND_MAX < 1);
 }
--- Chapter12-类和动态内存分配/example4/queue.cpp
+++ Chapter12-类和动态内存分配/example4/queue.cpp
@@ -1,22 +1,22 @@
 // queue.cpp -- Queue and Customer methods
 #include "queue.h"
-#include <cstdlib>         // (or stdlib.h) for rand()
+#include <cstdlib> // (or stdlib.h) for rand()
 
 // Queue methods
 Queue::Queue(int qs) : qsize(qs)
 {
-    front = rear = NULL;    // or nullptr
+    front = rear = NULL; // or nullptr
     items = 0;
 }
 
 Queue::~Queue()
 {
-    Node * temp;
-    while (front != NULL)   // while queue is not yet empty
+    Node *temp;
+    while (front != NULL) // while queue is not yet empty
     {
-        temp = front;       // save address of front item
-        front = front->next;// reset pointer to next item
-        delete temp;        // delete former front
+        temp = front;        // save address of front item
+        front = front->next; // reset pointer to next item
+        delete temp;         // delete former front
     }
 }
 
@@ -36,33 +36,33 @@ int Queue::queuecount() const
 }
 
 // Add item to queue
-bool Queue::enqueue(const Item & item)
+bool Queue::enqueue(const Item &item)
 {
     if (isfull())
         return false;
-    Node * add = new Node;  // create node
-// on failure, new throws std::bad_alloc exception
-    add->item = item;       // set node pointers
-    add->next = NULL;       // or nullptr;
+    Node *add = new Node; // create node
+                          // on failure, new throws std::bad_alloc exception
+    add->item = item;     // set node pointers
+    add->next = NULL;     // or nullptr;
     items++;
-    if (front == NULL)      // if queue is empty,
-        front = add;        // place item at front
+    if (front == NULL) // if queue is empty,
+        front = add;   // place item at front
     else
-        rear->next = add;   // else place at rear
-    rear = add;             // have rear point to new node
+        rear->next = add; // else place at rear
+    rear = add;           // have rear point to new node
     return true;
 }
 
 // Place front item into item variable and remove from queue
-bool Queue::dequeue(Item & item)
+bool Queue::dequeue(Item &item)
 {
     if (front == NULL)
         return false;
-    item = front->item;     // set item to first item in queue
+    item = front->item; // set item to first item in queue
     items--;
-    Node * temp = front;    // save location of first item
-    front = front->next;    // reset front to next item
-    delete temp;            // delete former first item
+    Node *temp = front;  // save location of first item
+    front = front->next; // reset front to next item
+    delete temp;         // delete former first item
     if (items == 0)
         rear = NULL;
     return true;
@@ -76,5 +76,5 @@ bool Queue::dequeue(Item & item)
 void Customer::set(long when)
 {
     processtime = std::rand() % 3 + 1;
-    arrive = when; 
+    arrive = when;
 }
