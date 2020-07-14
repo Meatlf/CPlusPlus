04aaf923770c9a2225cdc16daf2b4e6f8d49ade0
Meatlf <454779758@qq.com>
2020-07-14 21:35:57 +0800

    feat 第4章关于getline的例程 说明：对比cin和cin.getline()的区别

--- Chapter04-复合类型/instr2.cpp
+++ Chapter04-复合类型/instr2.cpp
@@ -1,18 +1,46 @@
 // instr2.cpp -- reading more than one word with getline
 #include <iostream>
-int main()
+
+using namespace std;
+
+void use_getline();
+void dont_use_getline();
+
+// 使用getline
+void use_getline()
 {
-    using namespace std;
     const int ArSize = 20;
     char name[ArSize];
     char dessert[ArSize];
 
     cout << "Enter your name:\n";
-    cin.getline(name, ArSize);  // reads through newline
+    cin.getline(name, ArSize); // reads through newline
     cout << "Enter your favorite dessert:\n";
     cin.getline(dessert, ArSize);
     cout << "I have some delicious " << dessert;
     cout << " for you, " << name << ".\n";
+}
+
+// 不使用getline
+void dont_use_getline()
+{
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name:\n";
+    cin >> name;
+    cout << "Enter your favorite dessert:\n";
+    cin >> dessert;
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+}
+
+int main()
+{
+    use_getline();
+    cout << endl;
+    dont_use_getline();
     // cin.get();
-    return 0; 
+    return 0;
 }
