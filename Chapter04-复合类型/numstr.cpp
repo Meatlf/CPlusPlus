30abda5791a029cd0e59c5ca0d9f05efc6036dc3
Meatlf <454779758@qq.com>
2020-07-18 15:17:37 +0800

    feat 学习了第4章的get()

--- Chapter04-复合类型/numstr.cpp
+++ Chapter04-复合类型/numstr.cpp
@@ -6,7 +6,7 @@ int main()
     cout << "What year was your house built?\n";
     int year;
     cin >> year;
-    // cin.get();
+    cin.get();
     cout << "What is its street address?\n";
     char address[80];
     cin.getline(address, 80);
