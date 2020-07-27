017d9b6be178e4e4041c58a6f5f08e683d48fc7a
Meatlf <454779758@qq.com>
2020-07-27 18:36:10 +0800

    feat 复习了第3章的强制类型转换

--- Chapter03-处理数据/typecast.cpp
+++ Chapter03-处理数据/typecast.cpp
@@ -10,16 +10,17 @@ int main()
     auks = 19.99 + 11.99;
 
     // these statements add values as int
-    bats = (int) 19.99 + (int) 11.99;   // old C syntax
-    coots = int (19.99) + int (11.99);  // new C++ syntax
+    bats = (int)19.99 + (int)11.99;  // old C syntax
+    coots = int(19.99) + int(11.99); // new C++ syntax
     cout << "auks = " << auks << ", bats = " << bats;
     cout << ", coots = " << coots << endl;
 
     char ch = 'Z';
-    cout << "The code for " << ch << " is ";    // print as char
-    cout << int(ch) << endl;                    // print as int
+    cout << "The code for " << ch << " is "; // print as char
+    cout << int(ch) << endl;                 // print as int
     cout << "Yes, the code is ";
-    cout << static_cast<int>(ch) << endl;       // using static_cast
-   // cin.get();
-    return 0; 
+    cout << static_cast<int>(ch) << endl; // using static_cast
+                                          // cin.get();
+
+    return 0;
 }
