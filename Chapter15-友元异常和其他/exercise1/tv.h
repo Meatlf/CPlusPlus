d73d94d783573020f4f800681658799d46725042
Meatlf <454779758@qq.com>
2020-07-30 17:37:01 +0800

    temp 复习友元类

---
+++ Chapter15-友元异常和其他/exercise1/tv.h
@@ -0,0 +1,72 @@
+// tv.h -- Tv and Remote classes
+#ifndef TV_H_
+#define TV_H_
+
+class Tv
+{
+public:
+    // 设置Remote类为TV类的友元类
+    friend class Remote; // Remote can access Tv private parts
+    enum
+    {
+        Off,
+        On
+    };
+    enum
+    {
+        MinVal,
+        MaxVal = 20
+    };
+    enum
+    {
+        Antenna,
+        Cable
+    };
+    enum
+    {
+        TV,
+        DVD
+    };
+
+    Tv(int s = Off, int mc = 125) : state(s), volume(5),
+                                    maxchannel(mc), channel(2), mode(Cable), input(TV) {}
+    // void onoff() { state = (state == On) ? Off : On; }
+    // 采用位的异或运算来翻转状态 by Meatlf
+    void onoff() { state ^= 1; }
+    bool ison() const { return state == On; }
+    bool volup();
+    bool voldown();
+    void chanup();
+    void chandown();
+    void set_mode() { mode = (mode == Antenna) ? Cable : Antenna; }
+    void set_input() { input = (input == TV) ? DVD : TV; }
+    void settings() const; // display all settings
+private:
+    int state;      // on or off
+    int volume;     // assumed to be digitized
+    int maxchannel; // maximum number of channels
+    int channel;    // current channel setting
+    int mode;       // broadcast or cable
+    int input;      // TV or DVD
+};
+
+class Remote
+{
+private:
+    int mode; // controls TV or DVD
+    int state;
+
+public:
+    friend class Tv;
+    Remote(int m = Tv::TV) : mode(m) {}
+    bool volup(Tv &t) { return t.volup(); }
+    bool voldown(Tv &t) { return t.voldown(); }
+    void onoff(Tv &t) { t.onoff(); }
+    void chanup(Tv &t) { t.chanup(); }
+    void chandown(Tv &t) { t.chandown(); }
+    // 唯一直接访问Tv成员的Remote方法是Remote::set_chan()，因此它是唯一需要作为友元的方法
+    void set_chan(Tv &t, int c) { t.channel = c; }
+    void set_mode(Tv &t) { t.set_mode(); }
+    void set_input(Tv &t) { t.set_input(); }
+};
+#endif
