86a6e09dfd054b16164947e2cbdcca1429f12cb0
Meatlf <454779758@qq.com>
2020-08-14 07:52:42 +0800

    feat 基于gdb调试了书本中迭代器例程

--- Chapter16-string类和标准模板/copyit.cpp
+++ Chapter16-string类和标准模板/copyit.cpp
@@ -15,8 +15,15 @@ int main()
     // create an ostream iterator
     ostream_iterator<int, char> out_iter(cout, " ");
     // copy from vector to output
+    // 该copy()算法的功能是将数据从一个容器复制到另外一个容器中
+    // copy()前2个参数为输入迭代器，第3个参数为输出迭代器
     copy(dice.begin(), dice.end(), out_iter);
     cout << endl;
+    // 使用匿名迭代器
+    copy(dice.begin(), dice.end(), 
+        ostream_iterator<int, char> (cout, " * "));
+    cout << endl;
+
     cout <<"Implicit use of reverse iterator.\n";
     copy(dice.rbegin(), dice.rend(), out_iter);
     cout << endl;
